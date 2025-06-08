96b6a2430fbc65e64d04e2b749f1fb4830fcc8d2
user.name "Muhammad Hassan <user.email "hassanhunter133@gamil.com>
2025-06-08 14:42:56 +0500

    index route added

---
+++ Social-media-scraper/sample.data.js
@@ -0,0 +1,31 @@
+const mockData = [
+    {
+        platform: 'Instagram',
+        username: 'natgeo',
+        postedAt: '2025-06-07T14:30:00Z',
+        caption: 'A stunning view from the top of the world. The colors of the sunset were unreal!',
+        likes: 1250345,
+        comments: 4500,
+        reposts: 12000
+    },
+    {
+        platform: 'X',
+        username: 'elonmusk',
+        postedAt: '2025-06-08T09:15:00Z',
+        caption: 'Starship launch is looking good for next week. Team is doing great work.',
+        likes: 450123,
+        comments: 22000,
+        reposts: 89000
+    },
+    {
+        platform: 'YouTube',
+        username: 'mkbhd',
+        postedAt: '2025-06-05T20:00:00Z',
+        caption: 'My review of the new transparent phone is finally live! Check it out.',
+        likes: 875000,
+        comments: 55000,
+        reposts: 15000 // In YouTube context, this might be 'Saves' to playlists
+    }
+];
+
+export default mockData;
